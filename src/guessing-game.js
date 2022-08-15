93200ce5b6ac0de6d2c6548c157cdc4bbcc58018
Artur Tarasau <tarasov.a.web@gmail.com>
2022-08-15 10:31:47 +0300

    complite task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min; 
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.round((this.min+this.max)/2);
+        console.log(this.res)
+        return this.res
     }
 
     lower() {
-
+        this.max = this.res
     }
 
     greater() {
-
+        this.min = this.res
     }
 }
 
